4e797d63e199bbf8309fc1feb732dcb4b04f06aa
Jochem Smit <jhsmit@gmail.com>
2024-09-24 13:21:26 +0200

    template and theme

--- examples/panel_app.py
+++ examples/panel_app.py
@@ -1,18 +1,27 @@
 import panel as pn
 from ipymolstar.panel import PDBeMolstar
 
+theme = "light" if pn.config.theme == "default" else "dark"
+molstar = PDBeMolstar(molecule_id="1qyn", theme=theme, width="1150px")
+parameters = pn.Param(
+    molstar,
+    parameters=[
+        "molecule_id",
+        "spin",
+        "visual_style",
+        "hide_water",
+    ],
+)
+settings = pn.Column(parameters)
+view = pn.Column(molstar, sizing_mode="stretch_width")
+
+template = pn.template.FastListTemplate(
+    title="MaterialTemplate",
+    sidebar=[settings],
+    main_max_width="1200px",
+)
+
+template.main.append(view)
+
 if pn.state.served:
-    molstar = PDBeMolstar(molecule_id="1qyn", theme="light")
-    parameters = pn.Param(
-        molstar,
-        parameters=[
-            "molecule_id",
-            "spin",
-            "hide_controls",
-            "visual_style",
-            "hide_water",
-        ],
-    )
-    settings = pn.Column(parameters)
-    view = pn.Column(molstar, width=500)
-    pn.Row(settings, view).servable()
+    template.servable()
