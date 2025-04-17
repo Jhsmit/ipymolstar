e6c83ffeaca71ea9a0c304e7d3b0b8484cd9c944
Jochem Smit <jhsmit@gmail.com>
2025-04-17 09:53:50 +0200

    remove check for molviewspec package on import
    molviewspec isnt a requirement. users can read msvj files and load into MolViewSpec without having the package installed.

--- src/ipymolstar/__init__.py
+++ src/ipymolstar/__init__.py
@@ -1,20 +1,5 @@
 from ipymolstar.pdbemolstar import PDBeMolstar
-
-try:
-    import molviewspec  # noqa: F401
-except ImportError:
-
-    class MolViewSpec:
-        def __init__(self, *args, **kwargs):
-            raise ImportError(
-                "The ipymolstar MolViewSpec requires the molviewspec \n"
-                "Python package which may be installed using pip with\n"
-                "    pip install molviewspec\n"
-                "Afterwards, you will need to restart your Python kernel."
-            )
-
-else:
-    from ipymolstar.molviewspec import MolViewSpec  # type: ignore
+from ipymolstar.molviewspec import MolViewSpec
 
 __version__ = "0.1.0"
 __all__ = ["MolViewSpec", "PDBeMolstar", "__version__"]
